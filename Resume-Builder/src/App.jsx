9f3dd7d8bc0262351692e9f5f9fa2fc4a16b976c
Your Name <sharmaanshul9796@gmail.com>
2024-02-01 22:50:18 +0530

    header

--- Resume-Builder/src/App.jsx
+++ Resume-Builder/src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <Header/>
-    </>
-  )
+  return <Header />;
 }
 
-export default App
+export default App;
